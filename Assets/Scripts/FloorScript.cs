587fc08039f4f6ab14f18d95f736e10333d21e3e
Poonnanun Poonnopathum <32285599+poonnanun@users.noreply.github.com>
2020-05-10 01:43:23 +0700

    Add shortest way display

--- Assets/Scripts/FloorScript.cs
+++ Assets/Scripts/FloorScript.cs
@@ -6,11 +6,14 @@ public class FloorScript : MonoBehaviour
 {
     public Material normal;
     public Material onHover;
+    public Material error;
+    public Material path;
     private Renderer rend;
     private GameManager gameManager;
-    // Start is called before the first frame update
+    private bool isPath;
     void Start()
     {
+        isPath = false;
         gameManager = FindObjectOfType<GameManager>();
         rend = GetComponent<Renderer>();
     }
@@ -24,13 +27,33 @@ public class FloorScript : MonoBehaviour
     }
 
     private void OnMouseExit() {
-        if (rend != null){
+        if (rend != null && !isPath){
             rend.material = normal;
+        }else if(rend != null && isPath){
+            rend.material = path;
         }
     }
     private void OnMouseDown() {
         if(gameManager.GetState() == 0 && gameManager.getWallMax() == false){
             gameManager.SelectWall(gameObject);
         }
+    }
+    public void ErrorPlace(){
+        if (rend != null){
+            rend.material = error;
+        }
+    }
+
+    public void PathEnable(){
+        if (rend != null){
+            rend.material = path;
+        }
+        isPath = true;
+    }
+    public void PathDisable(){
+        if (rend != null){
+            rend.material = normal;
+        }
+        isPath = false;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,11 +18,17 @@ public class GameManager : MonoBehaviour
     public Text currencyText;
     public Text wallDeployText;
     public GameObject gameOverPanel;
+    public GameObject wallPrefabs;
+    public GameObject positionTrigger;
     private SpawnScript spawnScript;
     private GameObject selectedFloor;
     private GameObject selectedWall;
     private Vector3 orginalPosition;
     private List<GameObject> monsters;
+    private Dictionary<int, bool> floorsPos;
+    private Dictionary<int, int> posParents;
+    private List<GameObject> highlightedPath;
+    private List<int> exploredFloors;
     private int state;
     private int wave;
     private int life;
@@ -34,8 +40,12 @@ public class GameManager : MonoBehaviour
 
     void Awake()
     {
-        InitializeValue();
         DeclareObject();
+        InitializeValue();
+    }
+
+    private void Start() {
+        FindShotestPath();
     }
     void FixedUpdate()
     {
@@ -59,12 +69,23 @@ public class GameManager : MonoBehaviour
         lifeText.text = life.ToString();
         waveText.text = wave.ToString();
         monsterLeftText.text = "0";
+        for(int x=0; x<9; x++){
+            for(int z=0; z<9; z++){
+                string tmp = x.ToString()+z.ToString();
+                floorsPos.Add(int.Parse(tmp), true);
+            }
+        }
+        print("List Count = "+floorsPos.Count);
         currencyText.text = currency.ToString();
         wallDeployText.text = string.Format("{0}/{1}", wallDeploy.ToString(), maxWallDeploy.ToString());
     }
     private void DeclareObject(){
+        floorsPos = new Dictionary<int, bool>();
+        posParents = new Dictionary<int, int>();
         spawnScript = FindObjectOfType<SpawnScript>();
         monsters = new List<GameObject>();
+        highlightedPath = new List<GameObject>();
+        exploredFloors = new List<int>();
     }
     public void SelectWall(GameObject floor){
         selectedFloor = floor;
@@ -79,16 +100,29 @@ public class GameManager : MonoBehaviour
     public void BuildWall(GameObject building){
         if(wallMax == false){
             Vector3 newPos = new Vector3(selectedFloor.transform.position.x, selectedFloor.transform.position.y+1, selectedFloor.transform.position.z);
-            GameObject tmp = Instantiate(building, newPos, selectedFloor.transform.rotation);
+           
             buildUI.transform.position =  orginalPosition;
+            if(building == wallPrefabs){
+                string tmp = Mathf.RoundToInt(selectedFloor.transform.position.x).ToString()+Mathf.RoundToInt(selectedFloor.transform.position.z).ToString();
+                floorsPos[int.Parse(tmp)] = false;
+                if(!FindShotestPath()){
+                    selectedFloor.GetComponent<FloorScript>().ErrorPlace();
+                    floorsPos[int.Parse(tmp)] = true;
+                }else{
+                    GameObject newObj = Instantiate(building, newPos, selectedFloor.transform.rotation);
+                    wallDeploy++;
+                    wallDeployText.text = string.Format("{0}/{1}", wallDeploy.ToString(), maxWallDeploy.ToString());
+                }
+            }else{
+                GameObject newObj = Instantiate(building, newPos, selectedFloor.transform.rotation);
+                wallDeploy++;
+                wallDeployText.text = string.Format("{0}/{1}", wallDeploy.ToString(), maxWallDeploy.ToString());
+            }
             state = 0;
-            wallDeploy++;
-            wallDeployText.text = string.Format("{0}/{1}", wallDeploy.ToString(), maxWallDeploy.ToString());
             if(wallDeploy == maxWallDeploy){
                 wallMax = true;
             }
         }
-        
     }
     public void BuildUnit(GameObject building){
         Vector3 newPos = new Vector3(selectedWall.transform.position.x, selectedWall.transform.position.y+1, selectedWall.transform.position.z);
@@ -160,5 +194,138 @@ public class GameManager : MonoBehaviour
     }
     public void MainMenu(){
         UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+    private bool FindShotestPath(){
+        exploredFloors.Clear();
+        posParents.Clear();
+        Queue<int> queue = new Queue<int>();
+        queue.Enqueue(40);
+        while(queue.Count < 1000 && queue.Count > 0){
+            // print("queue count = "+queue.Count);
+            int currPos = queue.Dequeue();
+            // print(" num = "+ currPos);
+            if(currPos == 48){
+                showShotestPath();
+                return true;
+            }else if(exploredFloors.Contains(currPos)){
+
+            }
+            else{
+                List<int> tmp = WalkAblePath(currPos);
+                if(tmp.Count != 0){
+                    foreach(int i in tmp){
+                        queue.Enqueue(i);
+                    }
+                }
+                exploredFloors.Add(currPos);
+            }
+        }
+        return false;
+    }
+    private void showShotestPath(){
+        foreach(GameObject d in highlightedPath){
+            d.GetComponent<FloorScript>().PathDisable();
+        }
+        highlightedPath.Clear();
+        List<int> path = new List<int>();
+        int finishPos = 48;
+        int curr = finishPos;
+        int count = 0;
+        while(curr != 40){
+            path.Add(curr);
+            curr = posParents[curr];
+            count++;
+            if(count > 1000){
+                break;
+            }
+        }
+        foreach(int i in path){
+            float z = (float)Mathf.Floor(i%10);
+            float x = (float)Mathf.Floor(i/10);
+            // print("X="+ x + ", Z=" +z);
+            positionTrigger.transform.position = new Vector3(x, -0.5f, z);
+            Collider[] hitColliders = Physics.OverlapSphere(positionTrigger.transform.position, 0.25f);
+            int c = 0;
+            while (c < hitColliders.Length)
+            {
+                highlightedPath.Add(hitColliders[c].gameObject);
+                c++;
+            }
+        }
+        positionTrigger.transform.position = new Vector3(0f, 150f, 0f);
+        foreach(GameObject d in highlightedPath){
+            d.GetComponent<FloorScript>().PathEnable();
+        }
+    }
+    public void addPath(GameObject path){
+        print(1);
+        highlightedPath.Add(path);
+    }
+    private List<int> WalkAblePath(int currPosition){
+        List<int> tmp = new List<int>();
+        if(!exploredFloors.Contains(currPosition+1)){
+            if(currPosition+1 == 48){
+                if(!posParents.ContainsKey(48))
+                    posParents.Add(48,currPosition);
+            }
+            if(floorsPos.ContainsKey(currPosition+1)){
+                if(floorsPos[currPosition+1] == true){
+                    tmp.Add(currPosition+1);
+                }
+            }
+        }else{
+            if(!posParents.ContainsKey(currPosition)){
+                posParents.Add(currPosition,currPosition+1);
+            }
+        }
+        if(!exploredFloors.Contains(currPosition-10)){
+            if(currPosition-10 == 48){
+                if(!posParents.ContainsKey(48))
+                    posParents.Add(48,currPosition);
+            }
+            if(floorsPos.ContainsKey(currPosition-10)){
+                if(floorsPos[currPosition-10] == true){
+                    tmp.Add(currPosition-10);
+                }
+            }
+        }else{
+            if(!posParents.ContainsKey(currPosition)){
+                posParents.Add(currPosition,currPosition-10);
+            }
+                
+        }
+        if(!exploredFloors.Contains(currPosition+10)){
+            if(currPosition+10 == 48){
+                if(!posParents.ContainsKey(48))
+                    posParents.Add(48,currPosition);
+            }
+            if(floorsPos.ContainsKey(currPosition+10)){
+                if(floorsPos[currPosition+10] == true){
+                    tmp.Add(currPosition+10);
+                }
+            }
+        }else{
+            if(!posParents.ContainsKey(currPosition)){
+                posParents.Add(currPosition,currPosition+10);
+            }
+                
+        }
+        if(!exploredFloors.Contains(currPosition-1)){
+            if(currPosition-1 == 48){
+                if(!posParents.ContainsKey(48))
+                    posParents.Add(48,currPosition);
+            }
+            if(floorsPos.ContainsKey(currPosition-1)){
+                if(floorsPos[currPosition-1] == true){
+                tmp.Add(currPosition-1);
+                }
+            }
+        }else{
+            if(!posParents.ContainsKey(currPosition)){
+                posParents.Add(currPosition,currPosition-1);
+            }
+        }
+        
+        return tmp;
     }
 }
